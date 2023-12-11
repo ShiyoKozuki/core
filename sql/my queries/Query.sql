8832063b095281603bb865557fe4c017fea83236
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-12-11 15:32:09 -0500

    Queries

---
+++ sql/my queries/Query.sql
@@ -0,0 +1,30 @@
+UPDATE world . spell_template SET procCharges = 100 WHERE entry = 20925;
+UPDATE world . spell_template SET procCharges = 100 WHERE entry = 20927;
+UPDATE world . spell_template SET procCharges = 100 WHERE entry = 20928;
+
+UPDATE world . spell_template SET procChance = 10 WHERE entry = 20127;
+UPDATE world . spell_template SET procChance = 10 WHERE entry = 20130;
+UPDATE world . spell_template SET procChance = 10 WHERE entry = 20135;
+UPDATE world . spell_template SET procChance = 10 WHERE entry = 20136;
+UPDATE world . spell_template SET procChance = 10 WHERE entry = 20137;
+UPDATE world . spell_proc_event SET procEx = 3 WHERE entry = 20127;
+
+UPDATE world . spell_template SET effect2 = 6 WHERE entry = 20189;
+UPDATE world . spell_template SET effectImplicitTargetA2 = 1 WHERE entry = 20189;
+UPDATE world . spell_template SET effectDieSides2 = 1 WHERE entry = 20189;
+UPDATE world . spell_template SET effectBaseDice2 = 1 WHERE entry = 20189;
+UPDATE world . spell_template SET effectApplyAuraName2 = 55 WHERE entry = 20189;
+
+UPDATE world . spell_template SET effect2 = 6 WHERE entry = 20192;
+UPDATE world . spell_template SET effectImplicitTargetA2 = 1 WHERE entry = 20192;
+UPDATE world . spell_template SET effectDieSides2 = 1 WHERE entry = 20192;
+UPDATE world . spell_template SET effectBaseDice2 = 1 WHERE entry = 20192;
+UPDATE world . spell_template SET effectBasePoints2 = 1 WHERE entry = 20192;
+UPDATE world . spell_template SET effectApplyAuraName2 = 55 WHERE entry = 20192;
+
+UPDATE world . spell_template SET effect2 = 6 WHERE entry = 20193;
+UPDATE world . spell_template SET effectImplicitTargetA2 = 1 WHERE entry = 20193;
+UPDATE world . spell_template SET effectDieSides2 = 1 WHERE entry = 20193;
+UPDATE world . spell_template SET effectBaseDice2 = 1 WHERE entry = 20193;
+UPDATE world . spell_template SET effectBasePoints2 = 2 WHERE entry = 20193;
+UPDATE world . spell_template SET effectApplyAuraName2 = 55 WHERE entry = 20193;
