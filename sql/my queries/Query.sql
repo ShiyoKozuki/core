fac336cb44b926c0f8e151aeb4140b73f7f03db3
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-12-12 13:47:07 -0500

    Custom queries / patch and docs

--- sql/my queries/Query.sql
+++ sql/my queries/Query.sql
@@ -28,3 +28,64 @@ UPDATE world . spell_template SET effectDieSides2 = 1 WHERE entry = 20193;
 UPDATE world . spell_template SET effectBaseDice2 = 1 WHERE entry = 20193;
 UPDATE world . spell_template SET effectBasePoints2 = 2 WHERE entry = 20193;
 UPDATE world . spell_template SET effectApplyAuraName2 = 55 WHERE entry = 20193;
+
+-- Silver Bronzed Set
+UPDATE world . item_template
+SET stat_type4 = 5,
+    stat_value4 = 3
+WHERE entry = 3482;
+
+UPDATE world . item_template
+SET stat_type4 = 5,
+    stat_value4 = 4
+WHERE entry = 2869;
+
+UPDATE world . item_template
+SET stat_type4 = 5,
+    stat_value4 = 3
+WHERE entry = 3483;
+
+UPDATE world . item_template
+SET stat_type4 = 5,
+    stat_value4 = 6
+WHERE entry = 10423;
+
+UPDATE world . item_template
+SET stat_type4 = 5,
+    stat_value4 = 3
+WHERE entry = 3481;
+
+-- Golden Scale Set
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 8
+WHERE entry = 3847;
+
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 10
+WHERE entry = 3837;
+
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 6
+WHERE entry = 3845;
+
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 4
+WHERE entry = 9366;
+
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 5
+WHERE entry = 3843;
+
+UPDATE world . item_template
+SET stat_type3 = 5,
+    stat_value3 = 6
+WHERE entry = 3841;
+
+
+
+
