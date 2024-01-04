81c4f991808741c89e419e47fdc370646b471652
Shiyo <strikinginsanity@hotmail.com>
2024-01-04 14:25:33 -0500

    Queries

--- sql/my queries/Creatures.sql
+++ sql/my queries/Creatures.sql
@@ -3,3 +3,11 @@
 -- Strath Gargoyles movement speed
 UPDATE `mangos`.`creature_template` SET `speed_run`=0.5 WHERE  `entry`=10408 AND `patch`=0;
 UPDATE `mangos`.`creature_template` SET `speed_run`=0.5 WHERE  `entry`=10409 AND `patch`=0;
+
+-- Bosses
+
+-- Balnazzar
+UPDATE `mangos`.`creature_template` SET `damage_multiplier`=5.0 WHERE  `entry`=10813;
+
+-- Kirtonos the Herald
+UPDATE `mangos`.`creature_template` SET `damage_multiplier`=5.0 WHERE  `entry`=10506;
