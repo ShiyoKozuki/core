da25c448aafb7084587789612fe09bfef4b30a41
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-02-15 13:02:18 -0500

    (Stealth) Stealth detection formula adjustment

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -6641,8 +6641,8 @@ bool Unit::CanDetectStealthOf(Unit const* target, float distance, bool* alert) c
     if (distance > sWorld.getConfig(IsPlayer() ? CONFIG_FLOAT_MAX_PLAYERS_STEALTH_DETECT_RANGE : CONFIG_FLOAT_MAX_CREATURES_STEALTH_DETECT_RANGE))
         return false;
 
-    float visibleDistance = IsPlayer() ? ((target->IsPlayer()) ? 9.0f : 21.f) : (5.0f / 6.0f);
-    float yardsPerLevel = IsPlayer() ? 1.5f : 5.0f / 6.0f;
+    float visibleDistance = IsPlayer() ? ((target->IsPlayer()) ? 9.0f : 21.f) : (9.0f);
+    float yardsPerLevel = IsPlayer() ? 1.5f : 5.0f / 3.0f;
     int32 stealthSkill = target->IsPlayer() ? target->GetTotalAuraModifier(SPELL_AURA_MOD_STEALTH) : target->GetLevelForTarget(this) * 5;
     stealthSkill += target->GetTotalAuraModifier(SPELL_AURA_MOD_STEALTH_LEVEL);
     int32 detectSkill = GetLevelForTarget(target) * 5 + int32(GetTotalAuraModifierByMiscValue(SPELL_AURA_MOD_STEALTH_DETECT, 0));
