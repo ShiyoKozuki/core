4abb9be89c831d56157d9d59163d5491179cc0eb
Shiyo <strikinginsanity@hotmail.com>
2025-02-22 22:23:47 -0500

    Queries

--- sql/my queries/Spells.sql
+++ sql/my queries/Spells.sql
@@ -4,7 +4,7 @@
 -- 15061 NEXT SKILL_LINE_ABILITY
 -- *****MUST UNLEARN ALL RANKS OF SPELLS(NOT JUST LOWEST) WHEN TESTING!!*****
 
--- Used with aurName HandleAuraModStat(29)
+-- Used with auraName HandleAuraModStat(29)
 -- Value for effetMiscValue
 -- enum Stats
 -- {
@@ -13,6 +13,39 @@
 --     STAT_STAMINA                       = 2,
 --     STAT_INTELLECT                     = 3,
 --     STAT_SPIRIT                        = 4
+-- };
+
+-- Used with auraName SPELL_AURA_ADD_PCT_MODIFIER = 108
+-- Value for effetMiscValue
+-- enum SpellModOp
+-- {
+--     SPELLMOD_DAMAGE                 = 0,
+--     SPELLMOD_DURATION               = 1,
+--     SPELLMOD_THREAT                 = 2,
+--     SPELLMOD_ATTACK_POWER           = 3,
+--     SPELLMOD_CHARGES                = 4,
+--     SPELLMOD_RANGE                  = 5,
+--     SPELLMOD_RADIUS                 = 6,
+--     SPELLMOD_CRITICAL_CHANCE        = 7,
+--     SPELLMOD_ALL_EFFECTS            = 8,
+--     SPELLMOD_NOT_LOSE_CASTING_TIME  = 9,
+--     SPELLMOD_CASTING_TIME           = 10,
+--     SPELLMOD_COOLDOWN               = 11,
+--     SPELLMOD_SPEED                  = 12,
+--     SPELLMOD_COST                   = 14,
+--     SPELLMOD_CRIT_DAMAGE_BONUS      = 15,
+--     SPELLMOD_RESIST_MISS_CHANCE     = 16,
+--     SPELLMOD_JUMP_TARGETS           = 17,
+--     SPELLMOD_CHANCE_OF_SUCCESS      = 18,                   // Only used with SPELL_AURA_ADD_FLAT_MODIFIER and affects proc spells
+--     SPELLMOD_ACTIVATION_TIME        = 19,
+--     SPELLMOD_EFFECT_PAST_FIRST      = 20,
+--     SPELLMOD_GLOBAL_COOLDOWN        = 21,
+--     SPELLMOD_DOT                    = 22,
+--     SPELLMOD_HASTE                  = 23,
+--     SPELLMOD_SPELL_BONUS_DAMAGE     = 24,
+--     SPELLMOD_MULTIPLE_VALUE         = 27,
+--     SPELLMOD_RESIST_DISPEL_CHANCE   = 28,
+--     MAX_SPELLMOD                    = 29,
 -- };
 
 -- For effectApplyAuraName1 13 and 135 all damage and healing is effectMiscValue1 126 (mask for all magic schools (non-physical))
