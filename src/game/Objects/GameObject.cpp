40e951860c1ac23da35fb4918b795d0245040ff9
Shiyo <strikinginsanity@hotmail.com>
2026-07-06 19:05:58 -0500

    Merge from development

--- src/game/Objects/GameObject.cpp
+++ src/game/Objects/GameObject.cpp
@@ -2257,7 +2257,7 @@ void GameObject::SetLootState(LootState state)
     }
 
     m_lootState = state;
-    UpdateCollisionState(false);
+    UpdateCollisionState();
 
     // Call for GameObjectAI script
     if (m_AI)
